e2673259bf953b20462da356f365684b8b9100dd
mirikoch <mirimail108@web.de>
2025-01-31 11:08:27 +0100

    Update vite.config.js

--- physiotherapy-app/vite.config.js
+++ physiotherapy-app/vite.config.js
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: "./",
   build: {
-    outDir: '../dist',
+    emptyOutDir: true,
   },
+  plugins: [vue()]
 })
